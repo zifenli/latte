eea8b30c5a18501965e359719c4a4aac67046186
lizifen <zifenli@gmail.com>
2018-05-21 14:06:41 +0800

    打印所有的key

--- static/main.js
+++ static/main.js
@@ -2,15 +2,16 @@
     var CHANNEL = 'pudding';
     var btn = $('#btn');
     var txt = $(':text');
-    var p = $('.j-flag');
-    var socket = io.connect('http://pusher.qcloudtest.cn/?channelId=' + CHANNEL);
+    var div = $('.j-flag');
+    // var socket = io.connect('http://pusher.qcloudtest.cn/?channelId=' + CHANNEL);
+    var socket = io.connect('http://localhost:3333/?channelId=' + CHANNEL);
     var url = '/api/data/proxy/';
 
     btn.click(function (event) {
         event.preventDefault();
         var data = {
             channel: CHANNEL,
-            value: txt.val()
+            url: txt.val()
         };
 
         $.ajax({
@@ -23,6 +24,14 @@
 
     socket.on('message', function(msg) {
         var msg = msg || {};
-        p.html('<h3>channel: '+ msg.channel +'</h3><h3>content: '+ msg.url +'</h3>');  
-    })
+        var html = '';
+        for (key in msg) {
+          html += '<p>' + key + ': ' + msg[key] + '</p>';
+        }
+        div.html(html);
+    });
+    // socket.on('message', function(msg) {
+    //     var msg = msg || {};
+    //     p.html('<h3>channel: '+ msg.channel +'</h3><h3>content: '+ msg.url +'</h3>');  
+    // })
 }());
